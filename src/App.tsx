8ff162509ffa73079157e8a2210e80c1ea2e9e89
Marcos Vinicius Maia da Silva <vinicius.maia@academico.ifrn.edu.br>
2022-08-03 14:19:55 -0300

    ícone para usuários e página com nome do usuário

--- src/App.tsx
+++ src/App.tsx
@@ -6,80 +6,107 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import ListSubheader from '@mui/material/ListSubheader';
 import Divider from '@mui/material/Divider';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+
 
 
 const App = () => {
 
-	
-	const Page = (value : any) => {
+    
+    const Page = (value : any) => {
 
-		const [mainUser, setMainUser] = useState('noone');
+        const [mainUser, setMainUser] = useState("noone");
 
-		const [page, setPage] = useState(0);
+        const [page, setPage] = useState(0);
 
-		const [loading, setLoading] = useState(true);
+        const [loading, setLoading] = useState(true);
 
-		const [users, setUsers] = useState([
-			{ id: 1, name: "Minora" },
-			{ id: 2, name: "Ataide" },
-		]);
+        const [users, setUsers] = useState([
+            { id: 1, name: "Minora" },
+            { id: 2, name: "Ataide" },
+        ]);
 
-		useEffect(() => {
-			fetch("https://jsonplaceholder.typicode.com/users/")
-				.then((response) => response.json())
-				.then((json) => { setUsers(json); setLoading(false) });
-		});
+        useEffect(() => {
+            fetch("https://jsonplaceholder.typicode.com/users/")
+                .then((response) => response.json())
+                .then((json) => { setUsers(json); setLoading(false) });
+        });
 
-		//Página inicial
-		if (page == 0) {
-			return (
-				(
-					<div className="App">
-						<div className="card">
-							{loading ? <h2>Carregando...</h2> : null}
-							<nav aria-label="secondary mailbox folders">
-								<List
-									sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-									component="nav"
-									aria-labelledby="Lista de usuários"
-									subheader={
-										<ListSubheader component="div" id="Lista de usuários">
-											Lista de usuários
-										</ListSubheader>
-									}
-								>
-									{users.map((user) => (
-										<ListItem disablePadding>
-											<ListItemButton onClick={() => {setPage(1); setMainUser(user.name)}}>
-												<ListItemText primary={user.name} />
-											</ListItemButton>
-										</ListItem>
-									))}
-								</List>
-							</nav>
-						</div>
-					</div>
-				)
-		)}
+        //Página inicial
+        if (page == 0) {
+            return (
+                (
+                    <div className="App">
+                        <div className="card">
+                            {loading ? <h2>Carregando...</h2> : null}
+                            <nav aria-label="secondary mailbox folders">
+                                <List
+                                    sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+                                    component="nav"
+                                    aria-labelledby="Lista de usuários"
+                                    subheader={
+                                        <ListSubheader component="div" id="Lista de usuários">
+                                            Lista de usuários
+                                        </ListSubheader>
+                                    }
+                                >
+                                    {users.map((user) => (
+                                        <ListItem disablePadding>
+                                            <ListItemButton onClick={() => {setPage(1); setMainUser(user.name)}}>
+                                                <ListItemAvatar>
+                                                    <Avatar alt={user.name} src="/static/images/avatar/1.jpg" />
+                                                </ListItemAvatar>
+                                                <ListItemText primary={user.name} />
+                                            </ListItemButton>
+                                        </ListItem>
+                                    ))}
+                                </List>
+                            </nav>
+                        </div>
+                    </div>
+                )
+        )}
 
-		//Página do usuário
-		if (page == 1) {
+        //Página do usuário
+        if (page == 1) {
 
-			return (
-					<div className="App">
-						<div className="card">
-							Página 1
-						</div>
-					</div>
-				)
-		}
-	}
+            return (
+                    <div className="App">
+                        <div className="card">
+                            <h1>{mainUser}</h1>
+                        </div>
+                    </div>
+                )
+        }
+    }
 
-	return (
-		<div className="App">
-			{Page(0)}
-		</div>
-	);
+    return (
+
+        <div className="App">
+
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Users
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+            <div>{Page(0)}</div>
+            
+        </div>
+    );
 };
 
-export default App; 
+export default App;
--- src/App_bkp.tsx
+++ src/App_bkp.tsx
@@ -10,114 +10,74 @@ import Divider from '@mui/material/Divider';
 
 const App = () => {
 
-	const [users, setUsers] = useState([
-		{ id: 1, name: "Minora" },
-		{ id: 2, name: "Ataide" },
-	]);
-  const [loading, setLoading] = useState(true);
-
-	useEffect(() => {
-		fetch("https://jsonplaceholder.typicode.com/users/")
-			.then((response) => response.json())
-			.then((json) => {setUsers(json); setLoading(false)});
-	});
 	
-	
-	return (
+	const Page = (value : any) => {
 
+		const [mainUser, setMainUser] = useState("noone");
 
-		<div className="App">
-			<div className="card">
-        {loading ? <h2>Carregando...</h2> : null}
-				
-				<nav aria-label="secondary mailbox folders">
-					<List 
-					      sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-						  component="nav"
-						  aria-labelledby="Lista de usuários"
-						  subheader={
-							<ListSubheader component="div" id="Lista de usuários">
-							  Lista de usuários
-							</ListSubheader>
-						  }
-					>
-					{users.map((user) => (
-						<ListItem disablePadding>
-							<ListItemButton>
-							<ListItemText primary={user.name}/>
-							</ListItemButton>
-						</ListItem>		
-						))}
-					</List>		
-				</nav>
-			</div>
-		</div>
-	);
-};
+		const [page, setPage] = useState(0);
 
-export default App; 
+		const [loading, setLoading] = useState(true);
 
-/*
-import { useState, useEffect } from "react";
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import ListSubheader from '@mui/material/ListSubheader';
-import Divider from '@mui/material/Divider';
+		const [users, setUsers] = useState([
+			{ id: 1, name: "Minora" },
+			{ id: 2, name: "Ataide" },
+		]);
 
+		useEffect(() => {
+			fetch("https://jsonplaceholder.typicode.com/users/")
+				.then((response) => response.json())
+				.then((json) => { setUsers(json); setLoading(false) });
+		});
 
-const App = () => {
-
-	const [users, setUsers] = useState([
-		{ id: 1, name: "Minora" },
-		{ id: 2, name: "Ataide" },
-	]);
-  const [loading, setLoading] = useState(true);
-
-	useEffect(() => {
-		fetch("https://jsonplaceholder.typicode.com/users/")
-			.then((response) => response.json())
-			.then((json) => {setUsers(json); setLoading(false)});
-	});
-	
-	const [page, setPage] = useState([
 		//Página inicial
-		(
-			<div className="App">
-				<div className="card">
-					{loading ? <h2>Carregando...</h2> : null}
-					
-					<nav aria-label="secondary mailbox folders">
-						<List 
+		if (page == 0) {
+			return (
+				(
+					<div className="App">
+						<div className="card">
+							{loading ? <h2>Carregando...</h2> : null}
+							<nav aria-label="secondary mailbox folders">
+								<List
 									sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-								component="nav"
-								aria-labelledby="Lista de usuários"
-								subheader={
-								<ListSubheader component="div" id="Lista de usuários">
-									Lista de usuários
-								</ListSubheader>
-								}
-						>
-						{users.map((user) => (
-							<ListItem disablePadding>
-								<ListItemButton>
-								<ListItemText primary={user.name}/>
-								</ListItemButton>
-							</ListItem>		
-							))}
-						</List>		
-					</nav>
-				</div>
-			</div>
-		)
-						
-	])
-	
+									component="nav"
+									aria-labelledby="Lista de usuários"
+									subheader={
+										<ListSubheader component="div" id="Lista de usuários">
+											Lista de usuários
+										</ListSubheader>
+									}
+								>
+									{users.map((user) => (
+										<ListItem disablePadding>
+											<ListItemButton onClick={() => {setPage(1); setMainUser(user.name)}}>
+												<ListItemText primary={user.name} />
+											</ListItemButton>
+										</ListItem>
+									))}
+								</List>
+							</nav>
+						</div>
+					</div>
+				)
+		)}
+
+		//Página do usuário
+		if (page == 1) {
+
+			return (
+					<div className="App">
+						<div className="card">
+							<h1>{mainUser}</h1>
+						</div>
+					</div>
+				)
+		}
+	}
+
 	return (
 		<div className="App">
-			{page[0]}
+			{Page(0)}
 		</div>
 	);
 };
