bf282e55adcb1916d41e04c90023d241046b694b
Marcos Vinicius Maia da Silva <vinicius.maia@academico.ifrn.edu.br>
2022-08-04 14:29:33 -0300

    Página secundária e utlização de novos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -17,32 +17,79 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import { Grid } from "@mui/material";
 
-useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users/")
-        .then((response) => response.json())
-        .then((json) => { setUsers(json); setLoading(false) });
-});
 
-const PageUserList = (props: any) => {
-    return <Typography variant="h6">Home</Typography>
-}
-
-const PageUserTask = (props: any) => {
-    return <Typography variant="h6">Tasks</Typography>
-}
-
-const PageShow = (props: any) => {
-    if (props.pageIndex === 1) {
-        return <PageUserList />
-    } else if (props.pageIndex === 2) {
-        return <PageUserTask />
-    }
-}
 
 const App = () => {
-    const [page, setPage] = useState(2);
 
+    const [mainUser, setMainUser] = useState("noone");
 
+    const [page, setPage] = useState(0);
+    
+    const PageUserList = (props: any) => {
+
+        const [loading, setLoading] = useState(true);
+
+        const [users, setUsers] = useState([
+            { id: 1, name: "Minora" },
+            { id: 2, name: "Ataide" },
+        ]);
+
+        useEffect(() => {
+            fetch("https://jsonplaceholder.typicode.com/users/")
+                .then((response) => response.json())
+                .then((json) => { setUsers(json); setLoading(false) });
+        });
+        
+        return (
+            <Grid>
+                <Grid>
+                    {loading ? <h2>Carregando...</h2> : null}
+                    <nav aria-label="secondary mailbox folders">
+                        <List  
+                            sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }} 
+                            component="nav"
+                            aria-labelledby="Lista de usuários"
+                            subheader={
+                                <ListSubheader component="div" id="Lista de usuários">
+                                    Lista de usuários
+                                </ListSubheader>
+                            }
+                        >
+                            {users.map((user) => (
+                                <ListItem disablePadding>
+                                    <ListItemButton onClick={() => {setPage(1); setMainUser(user.name)}}>
+                                        <ListItemAvatar>
+                                            <Avatar alt={user.name} src="/static/images/avatar/1.jpg" />
+                                        </ListItemAvatar>
+                                        <ListItemText primary={user.name} />
+                                    </ListItemButton>
+                                </ListItem>
+                            ))}
+                        </List>
+                    </nav>
+                </Grid>
+            </Grid>
+        )
+    }
+    
+    const PageUserTask = (props: any) => {
+        return (
+            <div className="App">
+                <div className="card">
+                    <h1>{mainUser}</h1>
+                </div>
+            </div>
+        )
+    }
+    
+    const PageShow = (props: any) => {
+        if (props.pageIndex === 0) {
+            return <PageUserList />
+        } else if (props.pageIndex === 1) {
+            return <PageUserTask />
+        }
+        return <></>
+    }
     
     // const Page = (value : any) => {
 
@@ -113,15 +160,15 @@ const App = () => {
 
     return (
         <Grid container>
-                <AppBar position="static">
-                    <Toolbar>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Users
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                    </Toolbar>
-                </AppBar>
-                <PageShow pageIndex={page} />
+            <AppBar position="static">
+                <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    Users
+                </Typography>
+                <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+            <PageShow pageIndex={page} />
         </Grid>
     );
 };
